04362fc602caeb468788c9bfe2e05d4a20c02e3d
oshry <oshrylevy@gmail.com>
2019-10-02 00:31:09 +0300

    fix landing

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
 
 class Landing extends Component {
@@ -11,13 +10,7 @@ class Landing extends Component {
                     <div className="container">
                         <div className="row">
                             <div className="col-md-12 text-center">
-                                <h1 className="display-3 mb-4">Developer Connector
-                                </h1>
-                                <p className="lead"> Create a developer profile/portfolio, share posts and get help from
-                                    other developers</p>
-                                <hr/>
-                                <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
-                                <Link to="/login" className="btn btn-lg btn-light">Login</Link>
+                                <h1 className="display-3 mb-4">Hello</h1>
                             </div>
                         </div>
                     </div>
