9bc0d154ce71c463368aaa81e98e676d9ee4cf32
oshry <oshrylevy@gmail.com>
2019-10-02 23:59:59 +0300

    add webpack

--- MY-README.md
+++ MY-README.md
@@ -36,3 +36,43 @@ ReactDOM.render(
     $ npm i --save react-lazy-load-image-component
     
    ref: [ https://www.npmjs.com/package/react-lazy-load-image-component ] 
+
+8. install webpack
+
+    $ npm i -D webpack webpack-cli webpack-dev-server html-webpack-plugin @babel/core @babel/preset-env babel-loader @babel/preset-react babel-plugin-transform-class-properties babel-plugin-transform-es2015-modules-commonjs
+    
+    extra packages:
+    
+    $ npm i -D babel-loader file-loader url-loader
+    
+    $ npm i -D image-webpack-loader
+                
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],
+            },
+    
+    npm i -D sass-loader css-loader style-loader
+    
+            {   
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+    
+    ref: [ https://blog.logrocket.com/versatile-webpack-configurations-for-your-react-application-e6ebf6615cc/ ]
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = ({ mode } = { mode: "production" }) => {
+    console.log(`mode is: ${mode}`);
+
+    return {
+        mode,
+        entry: "./src/index.js",
+        output: {
+            publicPath: "/",
+            path: path.resolve(__dirname, "build"),
+            filename: "bundled.js"
+        },
+        devServer: {
+            hot: true,
+            open: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jpe?g|png$/,
+                    exclude: /node_modules/,
+                    loader: ["url-loader", "file-loader"]
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
+                },
+                {
+                    test: /\.(gif|png|jpe?g|svg)$/i,
+                    use: [
+                        'file-loader',
+                        {
+                            loader: 'image-webpack-loader',
+                            options: {
+                                bypassOnDebug: true, // webpack@1.x
+                                disable: true, // webpack@2.x and newer
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        'style-loader',
+                        // Translates CSS into CommonJS
+                        'css-loader',
+                        // Compiles Sass to CSS
+                        'sass-loader',
+                    ],
+                },
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./public/index.html"
+            }),
+        ]
+    }
+};
