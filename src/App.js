a5ab27e1d9e9416cf9348c6b1b5a1d36ccbcd485
oshry <oshrylevy@gmail.com>
2019-10-12 15:03:34 +0300

    menu fix

--- src/App.js
+++ src/App.js
@@ -5,38 +5,15 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 import Navbar from './components/layout/Navbar'
-// import Content from "./components/layout/Content";
-import Landing from "./components/layout/Landing";
-import Photos from "./components/jph/Photos";
-import Search from "./components/jph/Search";
-import Animations from "./components/jph/Animation";
+import Content from "./components/layout/Content";
 
 function App() {
   return (
       <Provider store={ store }>
         <Router>
           <div className="wrapper">
-              <Navbar/>
-              <div id="content">
-                  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                      <div className="container-fluid">
-                          <button type="button" id="sidebarCollapse" onClick={
-                              ()=>{
-                                  var element = document.getElementById("sidebar");
-                                  element.classList.toggle("active");
-                              }
-                          } className="btn btn-info">
-                              <i className="fas fa-align-left"></i>
-                              <span>Toggle Sidebar</span>
-                          </button>
-
-                      </div>
-                  </nav>
-                  <Route exact path="/" component={Landing} />
-                  <Route exact path="/photos" component={Photos}/>
-                  <Route exact path="/search" component={Search}/>
-                  <Route exact path="/animations" component={Animations}/>
-              </div>
+            <Navbar/>
+            <Content/>
           </div>
         </Router>
       </Provider>
--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -11,7 +11,27 @@ class Content extends Component {
     componentDidMount() {}
     render() {
         return (
+            <div id="content">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container-fluid">
 
+                        <button type="button" id="sidebarCollapse" onClick={
+                            ()=>{
+                                var element = document.getElementById("sidebar");
+                                element.classList.toggle("active");
+                            }
+                        } className="btn btn-info">
+                            <i className="fas fa-align-left"></i>
+                            <span>Toggle Sidebar</span>
+                        </button>
+
+                    </div>
+                </nav>
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/photos" component={Photos}/>
+                    <Route exact path="/search" component={Search}/>
+                    <Route exact path="/animations" component={Animations}/>
+            </div>
         );
     }
 }
